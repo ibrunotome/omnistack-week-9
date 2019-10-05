2ebfae749d0a91deb02c4c5d6c96b132a69f406d
Bruno Tomé <ibrunotome@gmail.com>
2019-10-05 12:55:53 -0300

    Implement frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
 require('dotenv').config()
+const cors = require('cors')
+const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+
 const app = express()
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-iaygp.gcp.mongodb.net/omnistack9?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
 
+app.use(cors())
 app.use(express.json())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes)
 app.listen(3333)
